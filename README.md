a628098f5657c9f3b30b7b8bc62ec18c9b4eaa0c
Ankush-asabharwal <ankusha0209.be21@gmail.com>
2025-01-14 15:06:38 +0530

    [Ankusha] add: Adding use cases in README.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # line-comparison-problem-with-OOPS
+
+## UC1
+As a fan of geometry, I want to model a line based on a point consisting of (x,$
+- A Length as 2 Points (x1, y1) and (x2, y2)
+- Length of a Line = sqrt( (x2 - x1) ^ 2 + (y2 - y1) ^ 2)
+
+## UC2
+As a fan of geometry, I want to check equality of two lines based on the end po$
+
+## UC3
+As a fan of geometry, I want to compare two lines based on the end points, So t$
+
+
--- lineCompWithOOPS.cpp
+++ lineCompWithOOPS.cpp
@@ -75,6 +75,19 @@ int main(){
     delete l1;
     delete l2;
 }
+## line-comparison-problem
+
+## UC1
+As a fan of geometry, I want to model a line based on a point consisting of (x,$
+- A Length as 2 Points (x1, y1) and (x2, y2)
+- Length of a Line = sqrt( (x2 - x1) ^ 2 + (y2 - y1) ^ 2)
+
+## UC2
+As a fan of geometry, I want to check equality of two lines based on the end po$
+
+## UC3
+As a fan of geometry, I want to compare two lines based on the end points, So t$
+
 
 
 
